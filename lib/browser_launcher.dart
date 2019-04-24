6cf80f47f42f70b5fd39473070a87615065163ac
kenzieschmoll <kenzieschmoll@google.com>
2019-04-24 15:05:22 -0700

    review comments.

--- lib/browser_launcher.dart
+++ lib/browser_launcher.dart
@@ -1,5 +1,5 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 export 'src/chrome.dart';
